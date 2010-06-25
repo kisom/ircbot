a14e2fc1f0389286cce78162450594dc5f46ebeb
Kyle Isom <coder@kyleisom.net>
2010-06-25 01:37:52 -0600

    Basic shell laid out.

--- src/core.pl
+++ src/core.pl
@@ -26,6 +26,12 @@ use POE::Component::IRC;
 use POE::Component::IRC::State;
 use POE::Component::IRC::Plugin::AutoJoin;
 
+# variable declarations
+my $nickname    =   "awesomeo4000";
+my $username    =   "awesomeo4000";
+my $server      =   "irc.freenode.net";
+my @channels    =   ( "#awesomeotest" );
+
 # create session and list event handlers
 POE::Session -> create(
     package_states => [
@@ -33,4 +39,40 @@ POE::Session -> create(
     ]
 );
 
+# start the session
+$poe_kernel -> run();
+
+# start IRC session
+sub _start {
+    my $irc = POE::Component::IRC::State->spawn(
+        nick        => $nickname,
+        username    => $username,
+        server      => $server
+    );
+
+    $irc->plugin_add('AutoJoin', POE::Component::IRC::Plugin::AutoJoin->new(
+        channels => @channels ));
+
+    $irc->yield( join => $_ ) for @channels;
+    $irc->yield( 'connect' );
+
+    foreach (@channels) {
+        my $ch = $_;
+        $ch =~ s/^\#//;
+
+        # you can send mad gr33tz from here
+        
+    }
+}
+
+sub _ircjoin {
+    my $nick = (split /!/, $_[ARG0])[0];
+    my $channel = $_[ARG1];
+    my $irc = $_[SENDER]->get_heap();
+
+    # only send the message if we were the one joining
+    if ($nick eq $irc->$nickname) {
+        $irc->yield(privmsg => $channel, 'hai devious');
+    }
+}
 
