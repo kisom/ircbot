741874e0111c17281be52d4771a4eb777dcd2a3b
Kyle Isom <coder@kyleisom.net>
2012-02-07 21:59:43 +0300

    Logger is finished and working.

--- plugins/logger.py
+++ plugins/logger.py
@@ -12,26 +12,46 @@ import re
 
 
 LOGDIR = os.path.join(os.getcwd(), 'logs')
+SRVRE = r'^([\w.-])+ .+$'
 CHANRE = r'^.* PRIVMSG ([#-_\w]+) :.*$'
 MSGRE = r'^.* PRIVMSG [#-_\w]+ :(.+)$'
+
+
+def filename(server, channel):
+    """Build the filename for a log."""
+    servername = re.sub(r'^.+\.(\w+)\.\w{2,5}$', r'\1', server)
+    return os.path.join(LOGDIR, servername, '%s.log' % (channel, ))
+
+
+def log_message(server, channel, sender, message):
+    """Write a message to the appropriate log."""
+    path = filename(server, channel)
+
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+
+    logfile = open(filename(server, channel), 'a')
+    now = datetime.datetime.utcnow()
+
+    logstr = '%s %s: %s\n' % (now, sender, message)
+    logfile.write(logstr)
+    logfile.close()
+
 
 def main(data, conn):
     """
     Receive incoming messages and log them.
     """
 
-    log = open(LOGDIR + '/logger', 'w+')
-    log.write('DATA:\n%s\n-----\n' % (data,))
     data_arr = data.split(' ')
-    if data_arr[1] == 'PRIVMSG':
+    command = data_arr[1]
+
+    if command == 'PRIVMSG':
         sender = re.sub('^:(.+)\!.* PRIVMSG.*', '\\1', data).strip()
         channel = re.sub(CHANRE, r'\1', data).strip()
         message = re.sub(MSGRE, r'\1', data).strip()
+        server = conn.server
 
-        now = datetime.datetime.utcnow()
-        log.write('[*] %s logger: (%s) %s\n' % (now, channel, message))
-        conn.msg(channel, message)
-    log.close()
-
-
-        
+        if 0 == os.fork():
+            log_message(server, channel, sender, message)
+            os._exit(os.EX_OK)
