8c6789c2c87e3f65f8f5f8be96d9475b5185e62c
Kyle Isom <coder@kyleisom.net>
2012-02-07 19:16:19 +0300

    add debugging output

--- plugins/logger.py
+++ plugins/logger.py
@@ -20,14 +20,18 @@ def main(data, conn):
     Receive incoming messages and log them.
     """
 
+    log = open(LOGDIR + '/logger', 'w+')
+    log.write('DATA:\n%s\n-----\n' % (data,))
     data_arr = data.split(' ')
     if data_arr[1] == 'PRIVMSG':
         sender = re.sub('^:(.+)\!.* PRIVMSG.*', '\\1', data).strip()
         channel = re.sub(CHANRE, r'\1', data).strip()
         message = re.sub(MSGRE, r'\1', data).strip()
 
-        print '[*] logger: (%s) %s' % (channel, message)
+        now = datetime.datetime.utcnow()
+        log.write('[*] %s logger: (%s) %s\n' % (now, channel, message))
         conn.msg(channel, message)
+    log.close()
 
 
         
